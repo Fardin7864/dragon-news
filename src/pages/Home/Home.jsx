1d3a1d294dda7f6075a14c219a4a6e55e2e1e0ca
Fardin7864 <fardinhasan1888@gmail.com>
2023-10-03 11:44:34 +0600

    crate ad on right nav

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Marquee from "react-fast-marquee";
 import Navbar from "../../components/Navbar/Navbar";
 import Category from "../../components/Category/Category";
 import RightNav from "../../components/RightNav/RightNav";
+import adimg from "../../assets/bg.png"
 
 
 const Home = () => {
@@ -32,6 +33,18 @@ const Home = () => {
         </div>
         <div className="border-2">
             <RightNav></RightNav>
+            <div className="mt-5 w-full">
+              <div>
+                <img src={adimg} alt=""  className="w-full"/>
+              </div>
+              <div className="relative -top-96 text-center space-y-5 px-7">
+                <h2 className="text-3xl font-bold text-white">Create an <br /> Amazing <br /> Newspaper</h2>
+                <p className="text-white text-base">Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+                <button className=" h-12 bg-[#D72050] text-[#FFF] text-xl font-medium leading-7  px-3">
+                  Learn More
+                </button>
+              </div>
+            </div>
         </div>
       </div>
     </div>
